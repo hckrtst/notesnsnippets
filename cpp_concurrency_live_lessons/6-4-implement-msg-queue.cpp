39fb18fdbdf85ddb42eb84be4124f9eacc1e326f
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-07-10 20:42:26 -0700

    checkpoint

--- cpp_concurrency_live_lessons/6-4-implement-msg-queue.cpp
+++ cpp_concurrency_live_lessons/6-4-implement-msg-queue.cpp
@@ -13,76 +13,41 @@ namespace fs = std::filesystem;
 using std::future, std::cout, std::endl, std::vector, std::string;
 
 template<typename T>
-class FileMonitor {
+class MessageQueue {
     public:
-        void push_back(T file) {
+        void send(T msg) {
             // always have a named variable for the lock_guard otherwise it is constructed
             // and destroyed right away!!! this will defeat the purpose of this call
             std::lock_guard<std::mutex> lck(_mtx);
             // explicity move to avoid a copy into vector
-            _allFiles.push_back(std::move(file));
+            _messages.push_back(std::move(msg));
             // notify client waiting
             _cond.notify_one();
         }
-        /* no need for this
-        bool hasData() const {
-            std::lock_guard<std::mutex> lck(_mtx);
-            return !_allFiles.empty();
-        } */
-
-        T pop_back() {
+        
+        T receive() {
             //this lock will be  released at the same time we enter wait --- atomically
             std::unique_lock<std::mutex> lck(_mtx);
             // handle spurious wakeups
-            while (_allFiles.empty()) _cond.wait(lck);
-            // we can also do _cond.wait(lck, [this]{ return !_allFiles.empty(); })
+            while (_messages.empty()) _cond.wait(lck);
+            // we can also do _cond.wait(lck, [this]{ return !_messages.empty(); })
             // we have the locak again after this wait
-            T name = std::move(_allFiles.back());
-            _allFiles.pop_back();
+            T name = std::move(_messages.back());
+            _messages.pop_back();
             return name;
         }
         private:
-        std::deque<T> _allFiles;
+        std::deque<T> _messages;
         mutable std::mutex _mtx;
         mutable std::condition_variable _cond;
 };
 
-void listDir(string path, FileMonitor<string> & fileSink) {
-    for (fs::directory_iterator it(path); it != fs::directory_iterator(); ++it) {
-        if (fs::is_regular_file(it->status())) {
-            fileSink.push_back(it->path().filename());
-        }    
-    }
-}
-
-void listDirs(vector<fs::path> paths, FileMonitor<string> & fileSink) {
-    vector<future<void>> futures;
-
-    for (auto &path: paths) {
-        cout << path << endl;
-        futures.emplace_back(std::async(listDir, path, std::ref(fileSink)));
-    }
-
-    for(;;) {
-        //if (fileSink.hasData()) {
-            string name = fileSink.pop_back();
-            cout << "File : " << name << endl;
-        //}
-    }
-    for (auto& fut : futures) {
-        fut.wait();
-    }
-}
 
 int main() {
-    vector<fs::path> paths;
-    for (fs::directory_iterator it("/home/tintin/workspace"); it != fs::directory_iterator(); ++it) {
-        if (fs::is_directory(it->status())) {
-            paths.push_back(it->path());
-        }
-    }
-
-    FileMonitor<string> fileSink;
-    listDirs(paths, fileSink);
+    MessageQueue<int> queue;
+    std::future<void> fut = std::async([&queue]{ queue.send(44); });
+    int i = queue.receive();
+    cout << "Received: " << i << endl;
+    fut.wait();
 }
 

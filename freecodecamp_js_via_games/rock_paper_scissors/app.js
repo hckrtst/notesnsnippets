39d85b25c6077362003a7887298f6a13b663a101
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-07-16 10:05:08 -0700

    checkpoint

--- freecodecamp_js_via_games/rock_paper_scissors/app.js
+++ freecodecamp_js_via_games/rock_paper_scissors/app.js
@@ -8,6 +8,7 @@ choices.forEach(choice => choice.addEventListener(
     (e) => {
         user_choice_display.innerHTML = e.target.id
         genComputerChoice()
+        showResult()
     }
 )
 )
@@ -15,4 +16,34 @@ choices.forEach(choice => choice.addEventListener(
 function genComputerChoice() {
     const randomNum = Math.floor(Math.random() * choices.length)
     comp_choice_display.innerHTML = choices[randomNum].id
+}
+
+function showResult() {
+    let result
+    const computer_choice = comp_choice_display.innerHTML;
+    const user_choice = user_choice_display.innerHTML;
+    
+    if (computer_choice === user_choice) {
+        result_display.innerHTML = "Draw!!!"
+        return
+    }
+
+    const uwin_txt = 'You win!!!'
+    const iwin_txt = 'Computer wins!!!'
+    
+    switch(computer_choice) {
+        case "rock":
+            if (user_choice === 'paper') result = uwin_txt
+            else if (user_choice === 'scissors') result = iwin_txt
+            break            
+        case "scissors":
+            if (user_choice === 'paper') result = iwin_txt
+            else if (user_choice === 'rock') result = iwin_txt
+            break 
+        default:
+            if (user_choice === 'scissors') result = uwin_txt
+            else if (user_choice === 'rock') result = iwin_txt
+    }
+
+    result_display.innerHTML = result
 }

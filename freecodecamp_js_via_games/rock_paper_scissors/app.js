080fcd109cd1483385a49d1ca9dfa56ad56e9b5c
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-07-16 09:41:41 -0700

    checkpoint

--- freecodecamp_js_via_games/rock_paper_scissors/app.js
+++ freecodecamp_js_via_games/rock_paper_scissors/app.js
@@ -7,7 +7,12 @@ choices.forEach(choice => choice.addEventListener(
     'click',
     (e) => {
         user_choice_display.innerHTML = e.target.id
+        genComputerChoice()
     }
-    )
+)
 )
 
+function genComputerChoice() {
+    const randomNum = Math.floor(Math.random() * choices.length)
+    comp_choice_display.innerHTML = choices[randomNum].id
+}

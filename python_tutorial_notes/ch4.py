2a86a81b6b178dacf722ac58cffa35a605ef44f4
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-08-19 18:45:34 -0700

    checkpoint

--- python_tutorial_notes/ch4.py
+++ python_tutorial_notes/ch4.py
@@ -98,6 +98,18 @@ def cheese_shop(kind, *args, **kwargs):
 
 cheese_shop("Limburger", "It's very runny, sir.",
            "It's really very, VERY runny, sir.",
+           "...very runny indeed...",
            shopkeeper="Michael Palin",
            client="John Cleese",
            sketch="Cheese Shop Sketch")
+
+# As guidance:
+
+#     Use positional-only if you want the name of the parameters to not be available to the user.
+#     This is useful when parameter names have no real meaning, if you want to enforce the order of 
+#     the arguments when the function is called or if you need to take some positional parameters and arbitrary keywords.
+
+#     Use keyword-only when names have meaning and the function definition is more understandable by being explicit 
+# with names or you want to prevent users relying on the position of the argument being passed.
+
+#     For an API, use positional-only to prevent breaking API changes if the parameter’s name is modified in the future.

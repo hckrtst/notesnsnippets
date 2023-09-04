814e79c592466f75ac8dce9cc53b8848f24a52ef
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-09-04 02:25:42 -0700

    checkpoint

--- python_tutorial_notes/ch5.py
+++ python_tutorial_notes/ch5.py
@@ -5,4 +5,18 @@ print(stack)
 print(f"popped {stack.pop()}")
 print(f"top of stack = {stack[-1]}")
 
+# while lists can be used as queues, this is less efficient as
+# deleting from the front of a list is a O(n) operation
+# all elements have to be shifted by one
+# so use deque instead
+
+from collections import deque
+
+queue = deque(['eric', 'jim', 'kim'])
+queue.append('himesh')
+print(queue)
+print(f" queue pop = {queue.popleft()}")
+
+
+
 

131e87b263cf818513d9240976b8ff6d4a4297b8
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-10-06 00:47:49 -0700

    checkpoint

--- dj4e/simple_server.py
+++ dj4e/simple_server.py
@@ -2,13 +2,22 @@ import socket as so
 import time
 def createServer():
   server_sock = so.socket(so.AF_INET, so.SOCK_STREAM)
+  # makes sure we can re-use same port right away after shutting down
+  server_sock.setsockopt(so.SOL_SOCKET, so.SO_REUSEADDR, 1)
 
   try:
     server_sock.bind(('localhost', 9000))
     server_sock.listen(5)
     while 1:
       client_sock, address = server_sock.accept()
-      print(f'accepted from {client_sock}')
+      rd = client_sock.recv(5000).decode()
+      pieces = rd.split('\n')
+      if len(pieces) > 0: print(pieces[0])
+      data = "HTTP/1.1 200 OK\r\n"
+      data += 'Content-Type: text/html; charset=utf-8\r\n\r\n'
+      data += "<html><body>Hello from server</body></html>\r\n\r\n"
+      client_sock.sendall(data.encode())
+      client_sock.shutdown(so.SHUT_WR)
   except KeyboardInterrupt:
     print('interrupt detected...')
   

3f1dec024980c298b2548207f250949579e605e3
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-10-06 00:56:20 -0700

    Revert "checkpoint"
    This reverts commit a1b311f24d20b7c3d8e5288126e5f81d9489605e.

--- dj4e/simple_server.py
+++ dj4e/simple_server.py
@@ -4,7 +4,6 @@ def createServer():
   server_sock = so.socket(so.AF_INET, so.SOCK_STREAM)
   # makes sure we can re-use same port right away after shutting down
   server_sock.setsockopt(so.SOL_SOCKET, so.SO_REUSEADDR, 1)
-  hits = 0
 
   try:
     server_sock.bind(('localhost', 9000))
@@ -12,14 +11,11 @@ def createServer():
     while 1:
       client_sock, address = server_sock.accept()
       rd = client_sock.recv(5000).decode()
-      hits += 1
       pieces = rd.split('\n')
       if len(pieces) > 0: print(pieces[0])
       data = "HTTP/1.1 200 OK\r\n"
       data += 'Content-Type: text/html; charset=utf-8\r\n\r\n'
-      data += f'''<html><body>Hello from server<br>
-      <p>We have {hits} hits</p> 
-      </body></html>\r\n\r\n'''
+      data += "<html><body>Hello from server</body></html>\r\n\r\n"
       client_sock.sendall(data.encode())
       client_sock.shutdown(so.SHUT_WR)
   except KeyboardInterrupt:

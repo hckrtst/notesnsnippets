a344b3bc595b5c99ca8fe2a47962a944fa918f1e
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-10-26 20:14:35 -0700

    checkpoint

--- fastapi_experiments/main.py
+++ fastapi_experiments/main.py
@@ -6,6 +6,15 @@ app = FastAPI()
 async def root():
   return {'message', 'hello from fastapi :)'}
 
+
+# This works for both ints and floats!!!
+@app.get('/items/4')
+async def read_my_item():
+  print(f'I got exact item id')
+  # without explicitly converting it, this will actually return int!!!
+  # is this expected?
+  return {'my_item_id': 4}
+
 # This works for both ints and floats!!!
 @app.get('/items/{item_id}')
 async def read_my_item(item_id: float):

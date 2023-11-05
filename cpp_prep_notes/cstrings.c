124221962ab98dddfd78add4c1f00bf451c5c3ec
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-11-05 13:33:37 -0800

    checkpoint

--- cpp_prep_notes/cstrings.c
+++ cpp_prep_notes/cstrings.c
@@ -10,4 +10,10 @@ int main() {
   char num_str[] = "12345";
   printf("size of num_str is %d\n", sizeof(num_str));
   func(num_str);
+
+  char* ptr = num_str;
+  printf("size of ptr is %d, and size of *ptr is %d, size of &ptr is %d\n", sizeof(ptr), sizeof(&ptr));
+  char **ptr_ptr = &ptr;
+  printf("size of Ptr_ptr is %d\n", sizeof(ptr_ptr));
+
 }

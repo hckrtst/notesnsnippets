692d74f0a5b241a2022d52c3a5f9333b4e82307e
Sanket K <6290943+hckrtst@users.noreply.github.com>
2023-11-16 14:05:42 -0800

    checkpoint

---
+++ mastering-c-pointers/02_01-array1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[5] = { 11, 22, 33, 44, 55 };
+	int x;
+
+	int* p_a;
+
+	p_a = a; // we don't need to do &a since a is already a pointer
+
+	for( x=0; x<5; x++ )
+		printf("%d\n",p_a[x]);
+
+	return(0);
+}
